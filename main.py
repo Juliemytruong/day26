96baae24179ddc70eb6503f66c062191702bf529
julietruong <juliemytruong@gmail.com>
2024-06-21 21:55:32 +1000

    sdf

--- main.py
+++ main.py
@@ -1,25 +1,16 @@
-# with open("weather_data.csv","r") as weather:
-#     weather_week=weather.readlines()
-#     print(weather_week)
-#
-# import csv
-#
-# with open("weather_data.csv","r") as weather:
-#     weather_week=csv.reader(weather)
-#     temperatures=[]
-#     i=0
-#     for row in weather_week:
-#         if row[1]!="temp":
-#             temperatures.append(int(row[1]))
-#     print(temperatures)
-
 import pandas
-import statistics
 
-data=pandas.read_csv("weather_data.csv")
-data_dic=data.to_dict()
-print(data_dic)
-temp_list=data["temp"].to_list()
-print(temp_list)
-average_temp=statistics.mean(temp_list)
-print(average_temp)
+# with open("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv") as s_data:
+#     data=s_data.read()
+#     print(data)
+
+data=pandas.read_csv("2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+
+print(data["Primary Fur Color"])
+
+Gray_count=data["Primary Fur Color"].value_counts()["Gray"]
+Cinnamon_count=data["Primary Fur Color"].value_counts()["Cinnamon"]
+Black_count=data["Primary Fur Color"].value_counts()["Black"]
+print(Gray_count)
+print(Cinnamon_count)
+print(Black_count)
